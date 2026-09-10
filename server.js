ed1cbabd8966d232edf8e0d61ee022998ee7f2a2
luyinga <lumbaleluyinga2018@gmail.com>
2026-09-10 20:50:42 +0200

    Organizations updated

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.get('/categories', async (req, res) => {
 });
 
 app.listen(PORT, async () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
   try {
     await testConnection();
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
   } catch (error) {
     console.error('Error connecting to the database:', error);
